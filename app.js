1c865d36c4df617adde2e08d87c1c9badb662893
Shahlar95 <asahlar3@gmail.com>
2023-02-04 21:48:00 +0400

    Elementlerin secilmesi

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
-function myFunction() {
-    var x = document.getElementById("myInput");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
+// login parol sehifesinin secilmesi
+let main1 = document.querySelector(".main1");
+let login = document.querySelector(".login");
+let password = document.querySelector("password");
+let sign = document.querySelector("sign");
+let register = document.querySelector("register");
+
+function newlocat(newLogin) {
+
+    if ( localStorage.getItem(newLogin)===null) {
+
+        newLogin =[];
+        
+    }else{
+
+        
     }
-  }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function myFunction() {
+//     var x = document.getElementById("myInput");
+//     if (x.type === "password") {
+//       x.type = "text";
+//     } else {
+//       x.type = "password";
+//     }
+//   }
